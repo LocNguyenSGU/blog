370952faf70cbd12ec6b010a9ee02ab032828a82
LocNguyenSGU <lockbkbang@gmail.com>
2026-02-13 09:54:15 +0700

    Fix: Remove .vercel from git, let Vercel build fresh

--- .vercel/output/_functions/chunks/first-post.en_zmTeGrvB.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./first-post.en_DYfW3CQH.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
--- .vercel/output/_functions/chunks/markdown-guide.en_CuEyo1iX.mjs
+++
@@ -1,8 +0,0 @@
-async function getMod() {
-						return import('./markdown-guide.en_CWw-aQj8.mjs');
-					}
-					const collectedLinks = [];
-					const collectedStyles = [];
-					const defaultMod = { __astroPropagation: true, getMod, collectedLinks, collectedStyles, collectedScripts: [] };
-
-export { defaultMod as default };
